c159590f6079886c07ac36e311c34ccf301161bf
Alew <xalejandrow@gmail.com>
2023-09-09 18:49:11 -0300

    S3P18 - Reading Environment Variables

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+//  http://localhost:3000/api/v1/products
+app.get(api+'/', (req, res)=>{
     res.send('Hello API!');
 })
 
 app.listen(3000, () => {
+    console.log(api);
     console.log('server is running http://localhost:3000');
 })
