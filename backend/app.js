a30b0ee4e61ce089fd6ebdb4b8742e79bcf5df8c
Alew <xalejandrow@gmail.com>
2023-09-09 19:03:52 -0300

    S3P19 - Create First API Call and Parsing Json Data

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,23 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
+//Middleware
+app.use(express.json());
+
 //  http://localhost:3000/api/v1/products
-app.get(api+'/products', (req, res)=>{
-    res.send('Hello API!');
+app.get(`${api}/products`, (req, res)=>{
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3000, () => {
