e25b132fa25134f2311f64256c56950c85ef7066
Alew <xalejandrow@gmail.com>
2023-09-09 20:11:18 -0300

    S3P23 - Read/Write Data to Database Using API

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,42 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 
-//  http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req, res)=>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
     }
-    res.send(product);
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+//  http://localhost:3000/api/v1/products
+app.get(`${api}/products`, async (req, res)=>{
+    const productList = await Product.find();
+
+    if(!productList){
+        res.status(500).json({success: false})
+    }
+    res.send(productList);
 })
 
 app.post(`${api}/products`, (req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct=> {
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 })
 
 mongoose.connect(process.env.CONNECTION_STRING, {
