ca336bbe9ee628f0313f6ea1af4c58b26638ca37
Alew <xalejandrow@gmail.com>
2023-09-11 20:21:54 -0300

    S3P26 - Enabling CORS and Why Do Need It

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv/config');
 
-
+app.use(cors());
+app.options('*', cors());
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
