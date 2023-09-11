e68cedecddf7e3578e7f436ab2b7882980f405bd
Alew <xalejandrow@gmail.com>
2023-09-11 20:16:25 -0300

    S3P25 Create Backend API Routes and Schemas

--- backend/app.js
+++ backend/app.js
@@ -5,50 +5,24 @@ const mongoose = require('mongoose');
 
 require('dotenv/config');
 
-const api = process.env.API_URL;
 
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
+const api = process.env.API_URL;
 
-const Product = mongoose.model('Product', productSchema);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-//  http://localhost:3000/api/v1/products
-app.get(`${api}/products`, async (req, res)=>{
-    const productList = await Product.find();
-
-    if(!productList){
-        res.status(500).json({success: false})
-    }
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res)=>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -58,7 +32,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 .then(()=>{
     console.log('Database Connection is ready...')
 })
-.catch((erro)=>{
+.catch((err)=>{
     console.log(err);
 })
 
