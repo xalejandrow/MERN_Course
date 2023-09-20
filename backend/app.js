623ab996128d1498a18403b23b26bf57b465e9b3
Alew <xalejandrow@gmail.com>
2023-09-19 21:56:58 -0300

    S5P49 - Protecting the API and Authentication JWT Middleware

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,13 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-
+const authJwt = require('./helpers/jwt');
 app.use(cors());
 app.options('*', cors());
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
