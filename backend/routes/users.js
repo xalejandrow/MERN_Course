c98fe7e79b6b6c69e4e6dd68e0429c43747b9a61
Alew <xalejandrow@gmail.com>
2023-09-19 21:35:25 -0300

    S5P48 Login s User REST API and Creating a Token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) =>{
     const userList = await User.find().select('-passwordHash');
@@ -41,6 +42,28 @@ router.post('/', async (req,res)=>{
     return res.status(400).send('the user cannot be created!')
 
     res.send(user);
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({email: req.body.email})
+    const secret = process.env.secret;
+    if(!user){
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            secret,
+            {expiresIn: '1d'}
+        )
+        res.status(200).send({user: user.email, token: token})
+    }else{
+        res.status(400).send('password is wrong');
+    }
+
 })
 
 
