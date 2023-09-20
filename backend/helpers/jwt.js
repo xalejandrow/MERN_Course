1d78b8d89983606937a2f70babfb1e45525a4a7f
Alew <xalejandrow@gmail.com>
2023-09-19 22:37:16 -0300

    S5P51 - Excluding REST API Routes From Authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,9 +2,18 @@ const { expressjwt: expressJwt } = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
+    const api = process.env.API_URL;
     return expressJwt({
         secret,
         algorithms: ['HS256']
+    }).unless({
+        path: [
+            // {url: `${api}/products`, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`
+        ]
     })
 }
 
