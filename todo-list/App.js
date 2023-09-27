d7eb6d22b6482816ffbc0b85a3fac5a6a59955b5
Alew <xalejandrow@gmail.com>
2023-09-27 00:37:55 -0300

    S8P78 - Components - First App TodoList config

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TodoList from './Components/TodoList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+      <TodoList/>
+      /* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */
+      }
     </View>
   );
 }
---
+++ todo-list/Components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from "react-native";
+
+const TodoList = () => {
+    return(
+        <View>
+            <Text>Todo List</Text>
+        </View>
+    )
+}
+
+export default TodoList;
