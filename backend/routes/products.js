39cf537a0908dd8c0fac0298d67121e3bc0da003
Alew <xalejandrow@gmail.com>
2023-09-26 22:27:21 -0300

    S7P71 - Image Upload With Product Post Request

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -62,6 +62,10 @@ router.get(`/:id`, async (req, res) =>{
 router.post(`/`, uploadOptions.single('image'), async (req, res) =>{
     const category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid Category')
+
+    const file = req.file;
+    if(!file) return res.status(400).send('No image in the request')
+
     const fileName = req.file.filename
     const basePath = `${req.protocol}://${req.get('host')}/public/upload`;
 
