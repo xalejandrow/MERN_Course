bf42664aa65ff2f8f4493ebb7b300c4a392c3e68
Alew <xalejandrow@gmail.com>
2023-09-26 22:35:11 -0300

    S7P72 - Image Upload With Product PUT Request

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -91,20 +91,34 @@ router.post(`/`, uploadOptions.single('image'), async (req, res) =>{
     res.send(product);
 })
 
-router.put('/:id',async (req, res)=> {
+router.put('/:id',uploadOptions.single('image'), async (req, res)=> {
     if(!mongoose.isValidObjectId(req.params.id)) {
        return res.status(400).send('Invalid Product Id')
     }
     const category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid Category')
+   
+    const product = await Category.findById(req.params.id);
+    if(!product) return res.status(400).send('Invalid Product');
 
-    const product = await Product.findByIdAndUpdate(
+    const file = req.file;
+    let imagepath;
+
+    if(file){
+        const fileName = req.file.filename
+        const basePath = `${req.protocol}://${req.get('host')}/public/upload`;
+        imagepath = `${basePath}${fileName}`
+    }else{
+        imagepath = product.image;   
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
         req.params.id,
         {
             name: req.body.name,
             description: req.body.description,
             richDescription: req.body.richDescription,
-            image: req.body.image,
+            image: imagepath,
             brand: req.body.brand,
             price: req.body.price,
             category: req.body.category,
@@ -116,10 +130,10 @@ router.put('/:id',async (req, res)=> {
         { new: true}
     )
 
-    if(!product)
+    if(!updatedProduct)
     return res.status(500).send('the product cannot be updated!')
 
-    res.send(product);
+    res.send(updatedProduct);
 })
 
 router.delete('/:id', (req, res)=>{
