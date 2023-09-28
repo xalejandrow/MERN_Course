def3fec7715f2bb0eedcbf3c67d28f252bcff239
Alew <xalejandrow@gmail.com>
2023-09-27 23:03:26 -0300

    S9P87 - Intro and Folder Structure

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Screens
+import ProductContainer from './Screens/Products/ProductContainer'
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+const ProductContainer = () => {
+
+    return(
+        <View>
+            <Text>Product Container</Text>
+        </View>
+    )
+}
+
+export default ProductContainer;
