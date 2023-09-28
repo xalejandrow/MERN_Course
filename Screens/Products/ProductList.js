cd928481fe287789c0eae5c266bfa2666eadac1c
Alew <xalejandrow@gmail.com>
2023-09-28 01:09:05 -0300

    S9P90 Product Card

--- Screens/Products/ProductList.js
+++ Screens/Products/ProductList.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { TouchableOpacity, View, Dimensions } from "react-native";
+import { TouchableOpacity, View, Dimensions, Touchable } from "react-native";
+import ProductCard from "./ProductCard";
 
 var { width } = Dimensions.get("window");
 
 const ProductList = (props) => {
 
+    const {item} = props;
     return(
         <TouchableOpacity style={{ width: '50%'}}>
             <View style={{ width: width / 2,
                             backgroundColor: 'gainsboro'}}>
+            <ProductCard {...item}/>
 
             </View>
 
         </TouchableOpacity>
     )
 }
+
+export default ProductList;
