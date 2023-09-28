d252f1e77b0d7dcdaa40bc4d1964f3f8f6f2014e
Alew <xalejandrow@gmail.com>
2023-09-27 23:15:24 -0300

    S9P89 - FlatList Products

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+
+const data = require('../../assets/data/products.json');
 
 const ProductContainer = () => {
+
+    const [ products, setProducts ] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([])
+        }
+    }, [])
 
     return(
         <View>
             <Text>Product Container</Text>
+            <View style={{ marginTop: 100}}>
+            <FlatList
+                horizontal
+                data={products}
+                renderItem={({item}) => <Text>{item.brand}</Text>}
+                keyExtrator={item => item.name}
+            />
+            </View>
+            
         </View>
     )
 }
