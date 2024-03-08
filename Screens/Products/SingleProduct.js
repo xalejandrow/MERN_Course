29f92570e3e6c3bfacc77d59b408e0ccc40b61c5
Alew <xalejandrow@gmail.com>
2024-03-08 00:18:55 -0300

    S11P109 - Single Product

--- Screens/Products/SingleProduct.js
+++ Screens/Products/SingleProduct.js
@@ -21,7 +21,21 @@ const SingleProduct = (props) => {
 
                     />
                 </View>
+                <View style={styles.contentContainer}>
+                    <H1 style={styles.contentHeader}>{item.name}</H1>
+                    <Text style={styles.contentText}>{item.brand}</Text>
+                </View>
+                {/* TODO: Description, Rich Description and Availability */}
             </ScrollView>
+
+            <View style={styles.bottomContainer}>
+                <Left>
+                    <Text style={styles.price}>${item.price}</Text>
+                </Left>
+                <Right>
+                    <Button title="Add"/>
+                </Right>
+            </View>
         </Container>
     )
 }
@@ -37,9 +51,36 @@ const styles = StyleSheet.create({
         margin: 0
     },
     image: {
-        width: 250,
+        width: "100%",
         height: 250
+    },
+    contentContainer: {
+        marginTop: 20,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    contentHeader: {
+        fontWeight: "bold",
+        marginBottom: 20
+    },
+    contentText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 20
+    },
+    bottomContainer: {
+        flexDirection: "row",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        backgroundColor: "white"
+    },
+    price: {
+        fontSize: 24,
+        margin: 20,
+        color: "red"
     }
+
 })
 
 export default SingleProduct;
